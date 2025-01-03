9f8d142e11f2c3bff3c17783958f1ce688d7d9e9
Jonathan <j.sweet90@hotmail.com>
2025-01-02 22:09:15 -0600

    Add state flag and import ENEMYSTATES

--- src/squid.py
+++ src/squid.py
@@ -1,11 +1,14 @@
 from enum import Enum
 from actor import Actor
+from constants import ENEMYSTATES
 
 class Squid(Actor):
     def __init__(self):
         super().__init__()
 
         self.health = 1
+
+        self.current_state = ENEMYSTATES.WALKING
     
     # TODO Create and update where the squid simply walks left and right and turns around at walls
     # Should they care about ledges?
