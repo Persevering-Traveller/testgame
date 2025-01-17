f476ea3a54bbe74f58ee82692d5f3ac2a34edb9f
Jonathan <j.sweet90@hotmail.com>
2025-01-16 20:36:52 -0600

    Create Timer class

---
+++ src/timer.py
@@ -0,0 +1,34 @@
+import pygame
+import constants
+
+IN_SECONDS = 1000
+
+class Timer():
+    def __init__(self, id, end_time = 0):
+        self.id = id
+        self.started = False
+        self.accumulator = 0
+        self.end_time = end_time * IN_SECONDS
+    
+    def start(self):
+        self.started = True
+
+    def reset(self):
+        self.accumulator = 0
+    
+    def stop(self):
+        self.started = False
+        self.reset()
+    
+    def set_new_time(self, new_time):
+        self.stop()
+        self.end_time = new_time * IN_SECONDS
+    
+    def update(self, dt):
+        if not self.started: return
+        self.accumulator += dt
+
+        if self.accumulator >= self.end_time:
+            self.accumulator = 0
+            pygame.event.post(pygame.Event(constants.CUSTOMEVENTS.TIMER_ENDED, {"id": self.id}))
+            self.stop()
