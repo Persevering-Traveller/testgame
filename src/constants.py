6ae7f6ae6b6cf4f2a56d9e43fc252d2c1a5a0f96
Jonathan <j.sweet90@hotmail.com>
2025-01-17 16:05:51 -0600

    Add global TimerManager

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,14 @@
 from enum import Enum
 import pygame
+from manager_timer import TimerManager
 
 CANVAS_WIDTH = 160
 CANVAS_HEIGHT = 144
 TILE_SIZE = 16
 
 COLLISION_TILES = [39, 40, 41, 50, 52, 53] # The tile ID of each collidable tile in Tiled
+
+TIMER_MANAGER = TimerManager() # Only one timer manager and should be accessible to all enemies and player (or anything else)
 
 class GAMESTATE(Enum):
     TITLE = 0
