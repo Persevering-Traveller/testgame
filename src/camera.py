f1694c88a509560e9bf99f6cdd7db548c9909118
Jonathan <j.sweet90@hotmail.com>
2025-01-25 20:37:43 -0600

    Create initial Camera class

---
+++ src/camera.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Camera():
+    def __init__(self):
+        self.focused_entity = None # What the camera will 'follow'
+        self.level_entities_references = [] # All entities in the level, so it can shift them
+        self.level_tiles_reference = None
+    
+    def set_focus(self, entity):
+        self.focused_entity = entity
+    
+    def update(self, dt):
+        # TODO this should shift entities relative to the focused entity's position
+        for tile in self.level_tiles_reference:
+            tile.shift(-1, 0)
+
+        for entity in self.level_entities_references:
+            entity.shift(-1, 0)
