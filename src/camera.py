886ca8b68546aa90997802c96dca95a1de659a8b
Jonathan <j.sweet90@hotmail.com>
2025-01-31 18:42:37 -0600

    Add a level background reference  with a setter, and shift it in update

--- src/camera.py
+++ src/camera.py
@@ -5,6 +5,7 @@ class Camera():
         self.camera_target = None # What the camera will 'follow'
         self.level_entities_references = [] # All entities in the level, so it can shift them
         self.level_tiles_reference = None
+        self.level_background_reference = None
     
     def set_camera_target(self, target):
         self.camera_target = target
@@ -15,6 +16,9 @@ class Camera():
     def set_level_tiles(self, tiles):
         self.level_tiles_reference = tiles
     
+    def set_background_ref(self, bg):
+        self.level_background_reference = bg
+    
     def update(self, dt):
         # This perfectly follows the player
         # TODO Consider making a sort of buffer zone that's slightly left and slightly right of center
@@ -22,6 +26,9 @@ class Camera():
 
         for tile in self.level_tiles_reference:
             tile.shift(shift_amt, 0)
+        
+        # Shift background slightly slower than level for pretend depth
+        self.level_background_reference.shift(shift_amt/2, 0)
 
         for entity in self.level_entities_references:
             entity.shift(shift_amt, 0)
